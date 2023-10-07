085c7dff77973e7b9ced716ffc35121cf666b464
Sumit Singh <sumitsinghr3007@gmail.com>
2023-10-08 01:18:03 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Plaza_Hrms from './Components/PlazaHrms/Plaza_Hrms.js';
+
+function App() {
+  return (
+    <div>
+      <Plaza_Hrms/>
+    </div>
+  );
+}
+
+export default App;
